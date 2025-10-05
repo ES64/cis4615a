317a8d71c857fa9fbeaf549b64d50f1147a63ac6
ES64 <adrianjranger@hotmail.com>
2025-10-05 16:11:44 -0400

    created R10_THI00_J.java

--- R10_THI00_J10.java
+++ R10_THI00_J10.java
@@ -1,10 +1,12 @@
-public final class Foo implements Runnable {
+// THI00-J. Do not invoke Thread.run()
+
+public final class R10_THI00_J10 implements Runnable {
   @Override public void run() {
     // ...
   }
  
   public static void main(String[] args) {
     Foo foo = new Foo();
-    new Thread(foo).start();
+    new Thread(foo).run();
   }
 }
