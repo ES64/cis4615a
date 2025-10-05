54442ed34f2c182ef70d6c58ba6cca2daf885a49
ES64 <adrianjranger@hotmail.com>
2025-10-05 16:24:09 -0400

    Update R002_XP00_J.java

--- R002_XP00_J.java
+++ R002_XP00_J.java
@@ -1,3 +1,9 @@
+// EXP00-J. Do not ignore values returned by methods
+// Noncompliant Code Example (File Deletion)
+// Compliant Solution
+
+import java.io.File;
+
 public void deleteFile(){
  
   File someFile = new File("someFileName.txt");
